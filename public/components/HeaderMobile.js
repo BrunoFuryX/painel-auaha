35b870cef477d531fc890b8e5d8e220c3a491e35
Bruno Lopes <bruno.lopes@auaha.com.br>
2021-11-18 00:39:03 -0300

    up

--- public/components/HeaderMobile.js
+++ public/components/HeaderMobile.js
@@ -29,6 +29,7 @@ export default function HeaderMobile(props) {
 
 
     const LogoAuahaSvg = (props) => {
+        var color = props.color
         return (
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
             viewBox="0 0 2000 872.39" >
